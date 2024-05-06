30b0476cda6684630bd156d3dbe303bc1238d3ba
Srijith Santhosh <srijiths89@gmail.com>
2024-05-06 22:58:15 +0530

    Moving to react-router 6 from @reach/router

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,21 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Router, RouteComponentProps, Link } from "@reach/router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Projects from "./routes/projects";
-import { Redirect } from "@reach/router";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app" class="h-full"></div>';
-
-const ProjectsPage = (props: RouteComponentProps) => <Projects />;
-
-const ErrorPage = (props: RouteComponentProps) => <p>404</p>;
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
 root.render(
   <StrictMode>
-    <Router>
-      <ProjectsPage path="/projects" />
-      <Redirect from="/" to="/projects" />
-      <ErrorPage default />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/" element={<Navigate to="/projects" />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );

2ee4ecf67f9da3b25bd27a77ab715bc360e2d61a
Srijith Santhosh <srijiths89@gmail.com>
2024-05-06 23:59:57 +0530

    Setting up shadcn for components and creating projects page

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createProject } from "../api/takeone";
+import { Button } from "@/components/ui/button";
+import Sidebar from "./sidebar";
 
 export default function Project() {
   const handleCreateProject = async () => {
@@ -8,15 +10,33 @@ export default function Project() {
     window.alert(`Project created with ID: ${projectId}`);
   };
   return (
-    <div className="flex flex-col py-8 px-4 gap-8">
-      <div className="flex gap-16 justify-center">
-        <h1 className="text-3xl text-center">Hello Takeone</h1>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleCreateProject}
-        >
-          Create Project
-        </button>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Sidebar activePage="projects" />
+      <div className="flex flex-col">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <div className="flex items-center">
+            <h1 className="flex-1 text-lg font-semibold md:text-2xl">
+              Projects
+            </h1>
+            <Button onClick={handleCreateProject}>Create Project</Button>
+          </div>
+          <div
+            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+            x-chunk="dashboard-02-chunk-1"
+          >
+            <div className="flex flex-col items-center gap-1 text-center">
+              <h3 className="text-2xl font-bold tracking-tight">
+                You have no projects
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                Get started by creating a new project with AI
+              </p>
+              <Button className="mt-4" onClick={handleCreateProject}>
+                Create Project
+              </Button>
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
---
+++ src/routes/sidebar.tsx
@@ -0,0 +1,77 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import clsx from "clsx";
+import { Clapperboard, Home, ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+
+type Props = {
+  activePage: "projects" | "exports";
+};
+
+export default function Sidebar({ activePage }: Props) {
+  return (
+    <div className="border-r bg-muted/40">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <Link to="/" className="flex items-center gap-2 font-semibold">
+            <Clapperboard className="h-6 w-6" />
+            <span className="">TakeOne</span>
+          </Link>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <Link
+              to="/projects"
+              className={cn(
+                "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+                activePage === "projects" && "text-primary bg-muted"
+              )}
+            >
+              <Home className="h-4 w-4" />
+              Projects
+            </Link>
+            <Link
+              to="/exports"
+              className={cn(
+                "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+                activePage === "exports" && "text-primary bg-muted"
+              )}
+            >
+              <ShoppingCart className="h-4 w-4" />
+              Exports
+            </Link>
+          </nav>
+        </div>
+        <div className="border-t py-2">
+          <nav className="flex flex-col px-2 text-sm font-medium">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Link
+                  to="#"
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                >
+                  <ShoppingCart className="h-4 w-4" />
+                  Account
+                </Link>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
